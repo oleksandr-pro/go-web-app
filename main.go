c23817091501f6f4c74681e49baefebeca2c0f0c
chen <chengri1105@gmail.com>
2020-01-21 23:46:47 +0800

    course 2

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", helloHandler).Methods("GET")
+	r.HandleFunc("/goodbye", goodbyeHandler).Methods("GET")
+	http.Handle("/", r)
+	http.ListenAndServe(":8000", nil)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hellow world!")
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8000", nil)
+func goodbyeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Good bye!")
 }
